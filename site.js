67b1e83a12227d6b868863d587c94417c8cce5f3
Vlad Tcherkacheninov <tcherkv@vegacomputers.com>
2017-05-10 12:50:54 -0400

    upload keys

--- site.js
+++ site.js
@@ -157,7 +157,7 @@ function AddTerminal(termNum, connection)
         gateone.appendChild(container);
     else
         container = existingContainer;
-
+    GateOne.Terminal.getOpenTerminals()
     // Tell Gate One to create the new terminal
     if (termNum)
         GateOne.Terminal.newTerminal(termNum, null, container);
@@ -166,7 +166,15 @@ function AddTerminal(termNum, connection)
 
     GateOne.Terminal.switchTerminal(termNum);
     GateOne.Terminal.clearScrollback(termNum);
-    GateOne.ws.send(JSON.stringify({ 'terminal:ssh_save_known_hosts': get_identity_key() }));
+
+    GateOne.ws.send(JSON.stringify({ 'terminal:ssh_store_id_file':  {
+                        'name': 'cloud_deploy_id_rsa',
+                        'private': get_identity_key(),
+                    } }));
+    GateOne.ws.send(JSON.stringify({ 'terminal:ssh_store_id_file':  {
+                    'name': 'cloud_deploy_id_rsa',
+                    'public': get_identity_pub_key()
+                } }));
     GateOne.Terminal.sendString(connection.url, termNum);
 
     GateOne.Terminal.Input.capture(); // (re)start capturing keyboard input
@@ -209,21 +217,37 @@ function SelectTerminal(termNum)
 
 function get_identity_key(id) {
     return '-----BEGIN RSA PRIVATE KEY-----' +
-        'MIICXAIBAAKBgQCBwsgKzlDWd6vMUG+BkHM+5C1fdqdC6ZJw5bwKfi8kDA35feVY' +
-        'K8SeRBf4E7zuDojDFSK5aaMm7g5ICWQD5rqQtk6ZwVTFBBOmpiC/GUfupDwfO5GV' +
-        'jSzgVH6c2OA4Ou0gceE9P9Y8O6qvx3VodNq6xMukj0qijqSCRKpR4idL4wIDAQAB' +
-        'AoGAMuGP0zznUdGHGgyITMO5jPWoHQDHwWMZOw+6Vlr4XSgk6qnFHne7F1wYEfyb' +
-        'XrVwRx6oiQ+1G/TIwNJ911HH4ZVG/mIqsdX4PP64gj9WHuv2WUJi6ukvzNlya/os' +
-        'LwtZ7i5Mf55xnzd2byGLeRkz4JZXlmKGHi+H+EjIUu1xkTECQQD2OzBp2LMSD7D/' +
-        'nljypsG1G92D1ygrbfsu/YBhN+Qg7u/xxBa3yj+BcaF4E+8PmZCKnw2dKDryWaTl' +
-        'OPVvUo1HAkEAhuitsEEJO2nra4UopdveRj1kmMjwUcyzrvILbTxeoDqz1dvcfEeK' +
-        '6YuS9mU2pPDk80ON8G+VCkUWtLC9zFfqhQJAJWy/ogbeyMr7ww5lRJRV8toGZCiK' +
-        'oXc46156acxd7yeB7sQ4E704w8rqmN6mwj87+eXM4usfcoBvkKuEmmP4twJAXycd' +
-        '2oMEj/NVKLDyyokZbVE/8sNHPWq0EWCGzrUVyM2eqeg4yy9quu7G6SLaN9vwn+O1' +
-        '6S/KX0P7j9ZZCBe6QQJBAKCz8M9epVtX6zESZCK8PV2HfWjiFcU7EIM8JzGBrH+X' +
-        '9EaTtXHMcqMWRw6mxUR/CX9m4TPoDgKMpGMvABm8iNU=' +
-        '-----END RSA PRIVATE KEY-----'
+'MIIEowIBAAKCAQEAzpPwCB6A+HXHTS0E83ujlcLf4Dlz0DTo53SsGkJCQ7BiMwxh' +
+'FHpuUN3yUFTEgtTXLN+pDK+WiFJgELKCKH0NDxFPnezDE0iwwDsszD+5KfT+mxPP' +
+'ZLHUZQ5OvJ5oGSlYmrFhjTLqhrZTR1aa0Ml8/PvWbvu4MUuSK9JWGoa4wV5Og5GE' +
+'Gy4U0ytP9MkDiDUOligAUA1QaHUyorUIG41x9Is7E4/1epFpETtwgi+4keZIBAI9' +
+'4Cojd7f7tJk8WpmR18wchng8qLZ1PHLhncK5DwrYLcsK7P5WfOwm7fhVJW0oRZM4' +
+'GaC8IHtUO79WphUYcQ5VGqDP1z+fURZNUNL+tQIDAQABAoIBABtZwO0CROcgiOux' +
+'6zblhsocTBHe6XOGndOPQf4UCbv2jXneyLeyYgq46A00u/PqhDa/wh0Y3mBcL7Xm' +
+'PI9pxyHYk+b/4vAZrrU04ICGa1ifxFn57HqThOCAXXJhsryqGb4ZgCX2uNBvMWV/' +
+'lO3Rz4ebPE/jaga0z7yXk6jdb3bCbdOXsj0iPfHAuTZMJaVg43Mgr3Xmm8ozKGD4' +
+'4Htd8oMsmX26lgI4kH1pLP/wZHhVZmW5CHphizijkAaioPmPOH1JKl6hLNZ8SPWx' +
+'Lc4i6CDoyXk2k/YRHcUjyPDt6F97b96xZNXnyHENKhshci1ES2Htmq/0VQBqzq/m' +
+'Cm5KrYECgYEA5pLj5bav0fX6+K3tta+weFEQBLczp+cSZ5KwL0HQhtttR1/lEszp' +
+'Gz689ffmPw2FpaNLGk12o+HMOboWLiJICjeUg7hEoU0OLiIcAqX+nE+ptcGqEEpk' +
+'NxGP1yBZVkAxVIupAe8q+/O3gf7BGjvVqmJxoGFro/FPqowAFNUuatUCgYEA5Vuk' +
+'RWpowEDNPYyfD18vRMB48VeWQJm6SGsO+5kkggiIaOESIA0GT1ttvPpq3D5Ag13A' +
+'moSgoMV8kdpVg5QY8Oh7HE7CxcOnHS3s6PXMP1SysyLPx+Zl9zp1EAaJkT/usfDu' +
+'RHdTyDvw9OVaITElH5JfywLvkxQdmDoLvLyEdGECgYBo1DfQ5LIjH1avYURF6k6t' +
+'JQ3HVxOhLqlrdYGsupt39KNGZA7d8VO6E1gN7Fs+J4OvvwISP03FTI9xX21eJC/k' +
+'lUhsaoapX0MnLlLV0Wpk3sMl45rSNUBC2TUwdYCoeh7zdVutiXNdgXzZGV3p83I9' +
+'OOy4IhKyQrAZtfUDg7jK0QKBgQC543hUOlYD24F8rQgNcVviIQzkrdQ9L1rnbr2J' +
+'9Si9qyTDvAbwqx2+G5/pVhxdvKaU0dtUYMG/C9BWddpw9grw1btRyhPaLSS4ZYxp' +
+'ky2i9oaO4Vv97CIgKeTOxD2i12oiFd9G5l+lHc/A7FIMLMSlvgCdjReW4OrLgeAZ' +
+'a8D+gQKBgECz+ul/jNjYiNZ5nl47z2hqITd2M6fwBdH42A2ovgS+9a8zH2AUWFYx' +
+'gvPCic2hujZtjZcNQ1RvAb7eG2I3WU3DXUIkNw4iJdLk9j258iPVV1K0rfm9PoTv' +
+'NsvtJ40tn/X0uefyiO0CJDJTCgQqJkDH80SpeQbIpaqBjkHZoNPD' +
+'-----END RSA PRIVATE KEY-----'
 };
+
+function get_identity_pub_key(id) {
+    return 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOk/AIHoD4dcdNLQTze6OVwt/gOXPQNOjndKwaQkJDsGIzDGEUem5Q3fJQVMSC1Ncs36kMr5aIUmAQsoIofQ0PEU+d7MMTSLDAOyzMP7kp9P6bE89ksdRlDk68nmgZKViasWGNMuqGtlNHVprQyXz8+9Zu+7gxS5Ir0lYahrjBXk6DkYQbLhTTK0/0yQOINQ6WKABQDVBodTKitQgbjXH0izsTj/V6kWkRO3CCL7iR5kgEAj3gKiN3t/u0mTxamZHXzByGeDyotnU8cuGdwrkPCtgtywrs/lZ87Cbt+FUlbShFkzgZoLwge1Q7v1amFRhxDlUaoM/XP59RFk1Q0v61'
+}
 
 // -----BEGIN PUBLIC KEY-----
 // MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCBwsgKzlDWd6vMUG+BkHM+5C1f
